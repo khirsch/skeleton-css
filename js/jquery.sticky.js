4da903046f00f28638b1ada7cccef50552b8fdeb
Katrina Hirsch <katrina.hirsch@gmail.com>
2016-12-01 15:04:10 -0800

    fix mobile nav display

--- js/jquery.sticky.js
+++ js/jquery.sticky.js
@@ -34,7 +34,7 @@
       getWidthFrom: '',
       widthFromWrapper: true, // works only when .getWidthFrom is empty
       responsiveWidth: false,
-      zIndex: 'auto'
+      zIndex: '1'
     },
     $window = $(window),
     $document = $(document),
